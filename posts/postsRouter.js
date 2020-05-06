26d750e4182cf8703a3a69e1e1128d36d7b2ca62
Tanner Williams <tannerwill756@gmail.com>
2020-05-05 21:00:58 -0400

    finished mvp

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -1,7 +1,59 @@
 const router = require("express").Router();
 const db = require("../data/db.js");
 
-// POST title and contents
+//PUT update post by id
+router.put("/:id", (req, res) => {
+  db.findById(req.params.id)
+    .then((post) => {
+      if (post != 0) {
+        db.update(req.params.id, req.body).then((updPost) => {
+          if (req.body.title && req.body.contents) {
+            res.status(200).json(updPost);
+          } else {
+            res.status(400).json({
+              errorMessage: "Please provide title and contents for the post.",
+            });
+          }
+        });
+      } else {
+        res.status(404).json({
+          message: "couldnt find the post with that id",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: "The post information could not be modified.",
+      });
+    });
+});
+
+// DELETE a post
+router.delete("/:id", (req, res) => {
+  db.findById(req.params.id)
+    .then((post) => {
+      if (post != 0) {
+        db.remove(req.params.id)
+          .then((delPost) => {
+            res.status(200).json(delPost);
+          })
+          .catch((err) => {
+            console.log("uh oh");
+          });
+      } else {
+        res.status(404).json({
+          message: "couldnt find the post with that id",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: "The post could not be removed",
+      });
+    });
+});
+
+// POST a post
 router.post("/", (req, res) => {
   if (!req.body.title || !req.body.contents) {
     res.status(400).json({
@@ -22,24 +74,57 @@ router.post("/", (req, res) => {
   }
 });
 
-// // POST COMMENT
-// router.post("/:id/comments", (req, res) => {
-//   db.insertComment(req.body)
-//     .then((comment) => {
-//       res.status(200).json(comment);
-//     })
-//     .catch((err) => {
-//       res.status(500).json({
-//         error: "There was an error while saving the comment to the database",
-//       });
-//     });
-// });
+// POST COMMENT
+router.post("/:id/comments", (req, res) => {
+  db.findById(req.params.id).then((post) => {
+    if (post != 0) {
+      db.insertComment(req.body)
+        .then((comment) => {
+          if (req.body.text) {
+            res.status(201).json(comment);
+          } else {
+            res.status(400).json({
+              errorMessage: "Please provide text for the comment",
+            });
+          }
+        })
+        .catch((err) => {
+          res.status(500).json({
+            error:
+              "There was an error while saving the comment to the database",
+          });
+        });
+    } else {
+      res.status(404).json({
+        message: "couldnt find the post with that id",
+      });
+    }
+  });
+});
+
+// GET post by id
+router.get("/:id", (req, res) => {
+  db.findById(req.params.id)
+    .then((post) => {
+      if (post != 0) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({
+          message: "The post with that specified ID does not exist.",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: "The post information could not be retrieved.",
+      });
+    });
+});
 
 // GET COMMENTS
 router.get("/:id/comments", (req, res) => {
   db.findById(req.params.id)
     .then((post) => {
-      console.log(post.length);
       if (post != 0) {
         db.findPostComments(req.params.id)
           .then((comments) => {
